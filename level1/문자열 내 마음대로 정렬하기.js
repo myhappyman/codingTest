2fc6d6fff9fb183bdec6daf0d39b6a8aa2593389
Park_Shinwoo <myhappyman@naver.com>
2023-04-19 17:50:24 +0900

    자리 옮기고 주석 달아서 테스트중

--- level1/문자열 내 마음대로 정렬하기.js
+++ level1/문자열 내 마음대로 정렬하기.js
@@ -1,3 +1,10 @@
+/**
+ * n번째 위치한 문자열을 비교하여 정렬을 한다.
+ * n번쨰의 위치한 문자열이 같다면 문자열 전체를 비교하여 사전순으로 정렬한다.
+ * @param {*} strings 문자열이 담긴 배열
+ * @param {*} n 문자열에서 각 비교할 인덱스
+ * @returns 
+ */
 function solution(strings, n) {
   return [...strings].sort((a, b) => {
     if (a.charAt(n) === b.charAt(n)) {
