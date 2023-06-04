aeb087061e25c5e9abadd5ae33c19a3fba852a39
Park_Shinwoo <myhappyman@naver.com>
2023-06-05 00:08:03 +0900

    fix: :bug: 햄버거 만들기
    -정수형 배열을 slice하면 속도 이슈가 발생함
    -정수형 배열을 문자열로 변환시킨다
    -문자열을 시작인덱스부터 4개씩 체크해서 1231이 나오는지 체크한다
    -1231이 나올떄마다 카운트를 늘린다.
    -체크할 문자열도 해당 위치에서 4개를 잘라버린다. slice로 0부터 위치까지 위치부터 끝까지로 자름
    -위 행위를 반복한다

--- Programmers/level1/햄버거 만들기.js
+++ Programmers/level1/햄버거 만들기.js
@@ -1,51 +1,14 @@
 function solution(ingredient) {
     let cnt = 0;
     let str = ingredient.join("");
-    while (str.length > 4) {
-        const index = str.indexOf("1231");
-        if (index === -1) {
-            break;
+    for (let i = 0; i < str.length - 3; i++) {
+        if (str.slice(i, i + 4) === "1231") {
+            str = str.slice(0, i) + str.slice(i + 4);
+            cnt++;
+            i -= 4;
         }
-
-        str = str.slice(0, index) + str.slice(index + 4);
-        cnt++;
     }
     return cnt;
 }
-
-// function solution(ingredient) {
-//     let cnt = 0;
-//     let arr = [];
-//     for (let i = 0; i < ingredient.length; i++) {
-//         const num = ingredient[i];
-//         switch (num) {
-//             case 1:
-//                 if (arr.length === 0 || arr.length === 3) {
-//                     arr.push(num);
-//                     ingredient.splice(i--, 1);
-//                 }
-//                 break;
-//             case 2:
-//                 if (arr.length === 1) {
-//                     arr.push(num);
-//                     ingredient.splice(i--, 1);
-//                 }
-//                 break;
-//             case 3:
-//                 if (arr.length === 2) {
-//                     arr.push(num);
-//                     ingredient.splice(i--, 1);
-//                 }
-//                 break;
-//         }
-
-//         if (arr.length === 4) {
-//             arr = [];
-//             cnt++;
-//             i = i > 4 ? i - 4 : 0;
-//         }
-//     }
-//     return cnt;
-// }
 console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
 console.log(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]));
